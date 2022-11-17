f40ee243a2fa716b4c9b42635b7c275fe8d4abaf
alim1801 <Annabellim0118@gmail.com>
2022-11-17 01:18:33 -0800

    phase 3: moving everything to one frame

--- src/main/ui/StudentRecordFrame.java
+++ src/main/ui/StudentRecordFrame.java
@@ -1,19 +1,21 @@
 package ui;
 
+import model.Category;
 import model.Course;
 import model.StudentRecord;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
-public class StudentRecordFrame extends JFrame {
+public class StudentRecordFrame extends JFrame implements ActionListener {
     private static int id = 1; // id number of user
     private static final String JSON_STORE = "./data/studentRecord.json";
 
@@ -23,13 +25,221 @@ public class StudentRecordFrame extends JFrame {
     private JsonWriter jsonWriter;
     private JsonReader jsonReader;
 
+    private TextField catName;
+    private TextField catWeight;
+    private TextField catMark;
+    private TextField catStatus;
+
+    private TextField courseName;
+    private TextField courseCredits;
+    private TextField courseDesired;
+
+    private JButton buttonAdd;
+    private JButton buttonSubset;
+    private JButton next;
+    private JButton create;
+
+    private JRadioButton buttonAll;
+    private JRadioButton buttonComplete;
+    private JRadioButton buttonIncomplete;
+
+    private ButtonGroup buttonGroup;
+
+    private JPanel panel;
+    private JPanel addPanel;
+    private JPanel selectPanel;
+    private JPanel applyPanel;
+    private JPanel criteriaPanel;
+
+    private JLabel select;
+
+
     public StudentRecordFrame() {
+        super("Student Record");
+        this.setSize(500, 500);
+
         studentRecord = new StudentRecord(1);
         jsonWriter = new JsonWriter(JSON_STORE);
         jsonReader = new JsonReader(JSON_STORE);
+
+        // call on other methods here
+
+        doAdd();
+
+        doButton();
+
+        doPanel();
+
+        //add(panel);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
 
-    // category can have its own panel class
+    public void doAdd() {
+        buttonAdd = new JButton("Add Course");
+        buttonAdd.setActionCommand("Add");
+        buttonAdd.addActionListener(this);
+
+        addPanel = new JPanel();
+        addPanel.add(buttonAdd);
+    }
+
+    public void doButton() {
+        select = new JLabel("Sort courses by: ");
+        selectPanel = new JPanel();
+        selectPanel.add(select);
+
+        buttonGroup = new ButtonGroup();
+
+        buttonAll = new JRadioButton("All Courses");
+        buttonGroup.add(buttonAll);
+        buttonAll.setActionCommand("All");
+        buttonAll.addActionListener(this);
+
+        buttonComplete = new JRadioButton("Complete");
+        buttonGroup.add(buttonComplete);
+        buttonComplete.setActionCommand("Complete");
+        buttonComplete.addActionListener(this);
+
+        buttonIncomplete = new JRadioButton("Incomplete");
+        buttonGroup.add(buttonIncomplete);
+        buttonIncomplete.setActionCommand("Incomplete");
+        buttonIncomplete.addActionListener(this);
+
+        buttonSubset = new JButton("View");
+        buttonSubset.setActionCommand("View");
+        buttonSubset.addActionListener(this);
+        applyPanel = new JPanel();
+        applyPanel.add(buttonSubset);
+    }
+
+    public void doPanel() {
+        criteriaPanel = new JPanel();
+        criteriaPanel.setLayout(new FlowLayout());
+        criteriaPanel.add(buttonAll);
+        criteriaPanel.add(buttonComplete);
+        criteriaPanel.add(buttonIncomplete);
+
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(new EmptyBorder(new Insets(50, 50, 50, 50)));
+
+        panel.add(addPanel);
+        // move to student record panel
+        panel.add(selectPanel);
+        panel.add(criteriaPanel);
+        panel.add(applyPanel);
+    }
+
+    public void popUp() {
+        String num = JOptionPane.showInputDialog(this, "How many categories are in your course?", null);
+        int catNum = Integer.parseInt(num);
+        doCreateCategory(catNum);
+    }
+
+    // start with adding category class
+
+    public void doCreateCategory(int catNum) {
+        ArrayList<Category> allCategories = new ArrayList<>();
+
+        while (catNum >= 1) {
+            JPanel catPanel = new JPanel();
+            setLayout(new FlowLayout());
+            setPreferredSize(new Dimension(100, 100));
+            catHelper(catPanel);
+
+            allCategories.add(createCategory(getCatName(), getCatWeight(), getCatMark(), getCatStatus()));
+            catNum--;
+        }
+        doCreateCourse(allCategories);
+        createStudentRecord(id, allCourses);
+
+    }
+
+    private void doCreateCourse(ArrayList<Category> categories) {
+        JPanel coursePanel = new JPanel();
+        coursePanel.setLayout(new FlowLayout());
+        coursePanel.setPreferredSize(new Dimension(100, 100));
+        courseHelper(coursePanel);
+
+        Course temp = createCourse(getCourseName(), getCourseCredits(), getCourseDesired(), categories);
+        for (Category cat : categories) {
+            temp.addCategory(cat);
+        }
+        allCourses.add(temp);
+        studentRecord.addCourse(curCourse);
+
+    }
+
+
+    private Category createCategory(String name, double weight, double mark, boolean status) {
+        Category newCategory = new Category(name, weight, mark, status);
+        return newCategory;
+    }
+
+    private Course createCourse(String name, int credits, double desired, ArrayList<Category> categories) {
+        curCourse = new Course(name, credits, desired, categories);
+        return curCourse;
+    }
+
+    private void createStudentRecord(int id, List<Course> courses) {
+        studentRecord = new StudentRecord(id);
+
+        for (Course c : courses) {
+            studentRecord.addCourse(c);
+        }
+    }
+
+
+    public void catHelper(JPanel catPanel) {
+        catPanel.add(new Label("Category Name: "));
+        catName = new TextField(10);
+        catName.setEditable(true);
+        catPanel.add(catName);
+
+        catPanel.add(new Label("Category Weight: "));
+        catWeight = new TextField(10);
+        catWeight.setEditable(true);
+        catPanel.add(catWeight);
+
+        catPanel.add(new Label("Category Status (True/False): "));
+        catStatus = new TextField(10);
+        catStatus.setEditable(true);
+        catPanel.add(catStatus);
+
+        catPanel.add(new Label("Category mark (0 if incomplete): "));
+        catMark = new TextField(10);
+        catMark.setEditable(true);
+        catPanel.add(catMark);
+
+        next = new JButton("Next");
+        next.setActionCommand("Next");
+        next.addActionListener(this);
+        catPanel.add(next, BorderLayout.PAGE_END);
+    }
+
+    public void courseHelper(JPanel coursePanel) {
+        coursePanel.add(new Label("Course Name: "));
+        courseName = new TextField(10);
+        courseName.setEditable(true);
+        coursePanel.add(courseName);
+
+        coursePanel.add(new Label("Number of Credits: "));
+        courseCredits = new TextField(10);
+        courseCredits.setEditable(true);
+        coursePanel.add(courseCredits);
+
+        coursePanel.add(new Label("Desired Grade: "));
+        courseDesired = new TextField(10);
+        courseDesired.setEditable(true);
+        coursePanel.add(courseDesired);
+
+        create = new JButton("Create");
+        create.setActionCommand("Create");
+        create.addActionListener(this);
+        add(create, BorderLayout.PAGE_END);
+    }
 
 
 
@@ -45,6 +255,52 @@ public class StudentRecordFrame extends JFrame {
         }
     }
 
+    public String getCatName() {
+        return catName.getText();
+    }
 
+    public double getCatWeight() {
+        return Double.parseDouble(catWeight.getText());
+    }
 
+    public boolean getCatStatus() {
+        return Boolean.parseBoolean(catStatus.getText());
+    }
+
+    public double getCatMark() {
+        return Double.parseDouble(catMark.getText());
+    }
+
+    public String getCourseName() {
+        return courseName.getText();
+    }
+
+    public int getCourseCredits() {
+        return Integer.parseInt(courseCredits.getText());
+    }
+
+    public double getCourseDesired() {
+        return Double.parseDouble(courseDesired.getText());
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String button = e.getActionCommand();
+
+        // triggers creating category
+        if (button.equals("Add")) {
+            popUp();
+            //add(panel);
+
+            // after category is inputted
+        } else if (button.equals("Next")) {
+            //createCategory();
+            //setVisible(false);
+
+            // after course is inputted
+        } else if (button.equals("Create")) {
+            //createCourse();
+            //setVisible(false);
+        }
+    }
 }
--- src/main/ui/StudentRecordPanel.java
+++ src/main/ui/StudentRecordPanel.java
@@ -1,191 +1,190 @@
-package ui;
-
-import model.Course;
-import model.StudentRecord;
-import persistence.JsonReader;
-import persistence.JsonWriter;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.io.FileNotFoundException;
-import java.util.List;
-
-
-// shows course, credit, grade on screen
-// for returning user -> import from persistence package
-public class StudentRecordPanel extends JPanel implements ActionListener {
-    private static int id = 1;
-    private static final String JSON_STORE = "./data/studentRecord.json";
-
-    private JsonReader jsonReader;
-    private JsonWriter jsonWriter;
-
-    private JList<Course> courseJList;
-    private StudentRecord studentRecord;
-
-    private List<Course> courses;
-
-    private JPanel recordPanel;
-    private JPanel headingPanel;
-    private JLabel heading;
-    private JLabel courseName;
-    private JLabel credits;
-    private JButton exit;
-
-
-    public StudentRecordPanel(List<Course> courses) {
-        jsonReader = new JsonReader(JSON_STORE);
-        jsonWriter = new JsonWriter(JSON_STORE);
-
-
-        this.courses = courses;
-        createStudentRecord();
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setPreferredSize(new Dimension(100, 100));
-
-        // overall panel should be in card layout
-        // title: student record - id:
-        // each course should be in flow layout (course name - credits)
-        // "back" button
-
-        // want to create a student record here
-        heading = new JLabel();
-        // fix heading to be centered
-        heading.setHorizontalAlignment(SwingConstants.CENTER);
-        heading.setText("Student Record for id: " + studentRecord.getId());
-        heading.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
-
-        courseName = new JLabel();
-        courseName.setText("Course");
-        courseName.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        credits = new JLabel();
-        credits.setText("Credits");
-        credits.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-
-        headingPanel = new JPanel();
-        headingPanel.setLayout(new GridLayout(1, 2));
-        headingPanel.add(courseName);
-        courseName.setPreferredSize(new Dimension(50, 1));
-        courseName.setHorizontalAlignment(JLabel.CENTER);
-        headingPanel.add(credits);
-        credits.setSize(new Dimension(50, 1));
-        credits.setHorizontalAlignment(JLabel.CENTER);
-
-        exit = new JButton("Save and Exit");
-        exit.setActionCommand("Exit");
-        exit.addActionListener(this);
-
-
-        add(heading);
-        add(headingPanel);
-
-
-    }
-
-
-    public void createStudentRecord() {
-        studentRecord = new StudentRecord(id);
-
-        for (Course c : courses) {
-            studentRecord.addCourse(c);
-        }
-    }
-
-    public void viewAll() {
-        List<Course> courses = studentRecord.getCourseList();
-        for (Course c : courses) {
-            printCourse(c);
-
-        }
-        add(exit);
-        exit.setHorizontalAlignment(JButton.CENTER);
-    }
-
-    public void viewCompleted() {
-        List<Course> courses = studentRecord.getCourseList();
-        for (Course c : courses) {
-            c.checkIsCompleted();
-            if (c.getIsCompleted()) {
-                printCourse(c);
-            }
-        }
-        add(exit);
-        exit.setHorizontalAlignment(JButton.CENTER);
-    }
-
-    public void viewIncomplete() {
-        List<Course> courses = studentRecord.getCourseList();
-        for (Course c : courses) {
-            c.checkIsCompleted();
-            if (!c.getIsCompleted()) {
-                printCourse(c);
-            }
-        }
-        add(exit);
-        exit.setHorizontalAlignment(JButton.CENTER);
-    }
-
-    public void printCourse(Course c) {
-        recordPanel = new JPanel();
-        recordPanel.setLayout(new GridLayout(1, 2));
-
-
-        JLabel curName = new JLabel();
-        curName.setText(c.getCourseName());
-
-        JLabel curCredits = new JLabel();
-        curCredits.setText(Integer.toString(c.getCredits()));
-
-        recordPanel.add(curName);
-        curName.setPreferredSize(new Dimension(50, 1));
-        curName.setHorizontalAlignment(JLabel.CENTER);
-        recordPanel.add(curCredits);
-        curCredits.setPreferredSize(new Dimension(50, 1));
-        curCredits.setHorizontalAlignment(JLabel.CENTER);
-
-        add(recordPanel);
-        add(new JSeparator());
-
-
-    }
-
-
-    // saves student record before exiting
-//    public void saveBeforeExit() {
-//        addWindowListener(new WindowAdapter() {
-//            public void windowClosing(WindowEvent e) {
-//                try {
-//                    jsonWriter.open();
-//                    jsonWriter.write(studentRecord);
-//                    jsonWriter.close();
-//                } catch (FileNotFoundException exception) {
-//                    System.out.println("Unable to write to file: " + JSON_STORE);
-//                }
-//            }
-//        });
+//package ui;
+//
+//import model.Course;
+//import model.StudentRecord;
+//import persistence.JsonReader;
+//import persistence.JsonWriter;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.*;
+//import java.io.FileNotFoundException;
+//import java.util.List;
+//
+//
+//// shows course, credit, grade on screen
+//// for returning user -> import from persistence package
+//public class StudentRecordPanel extends JPanel implements ActionListener {
+//    private static int id = 1;
+//    private static final String JSON_STORE = "./data/studentRecord.json";
+//
+//    private JsonReader jsonReader;
+//    private JsonWriter jsonWriter;
+//
+//    private StudentRecord studentRecord;
+//    private List<Course> courses;
+//    private Course curCourse;
+//
+//    private JPanel recordPanel;
+//    private JPanel headingPanel;
+//    private JLabel heading;
+//    private JLabel courseName;
+//    private JLabel credits;
+//    private JButton exit;
+//
+//
+//    public StudentRecordPanel(List<Course> courses) {
+//        jsonReader = new JsonReader(JSON_STORE);
+//        jsonWriter = new JsonWriter(JSON_STORE);
+//        studentRecord = new StudentRecord(id);
+//
+//
+//        this.courses = courses;
+//        createStudentRecord();
+//        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+//        setPreferredSize(new Dimension(100, 100));
+//
+//        // overall panel should be in card layout
+//        // title: student record - id:
+//        // each course should be in flow layout (course name - credits)
+//        // "back" button
+//
+//        // want to create a student record here
+//        heading = new JLabel();
+//        // fix heading to be centered
+//        heading.setHorizontalAlignment(SwingConstants.CENTER);
+//        heading.setText("Student Record for id: " + studentRecord.getId());
+//        heading.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+//
+//        courseName = new JLabel();
+//        courseName.setText("Course");
+//        courseName.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+//        credits = new JLabel();
+//        credits.setText("Credits");
+//        credits.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+//
+//        headingPanel = new JPanel();
+//        headingPanel.setLayout(new GridLayout(1, 2));
+//        headingPanel.add(courseName);
+//        courseName.setPreferredSize(new Dimension(50, 1));
+//        courseName.setHorizontalAlignment(JLabel.CENTER);
+//        headingPanel.add(credits);
+//        credits.setSize(new Dimension(50, 1));
+//        credits.setHorizontalAlignment(JLabel.CENTER);
+//
+//        exit = new JButton("Save and Exit");
+//        exit.setActionCommand("Exit");
+//        exit.addActionListener(this);
+//
+//
+//        add(heading);
+//        add(headingPanel);
+//
 //
 //    }
-
-    public void saveBeforeExit() {
-        try {
-            jsonWriter.open();
-            jsonWriter.write(studentRecord);
-            jsonWriter.close();
-        } catch (FileNotFoundException exception) {
-            System.out.println("Unable to write to file: " + JSON_STORE);
-        }
-    }
-
-
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String button = e.getActionCommand();
-
-        if (button.equals("Exit")) {
-            saveBeforeExit();
-            setVisible(false);
-        }
-    }
-}
+//
+//
+//    public void createStudentRecord(int id, List<Course> courseList) {
+//        studentRecord = new StudentRecord(id);
+//        for (Course c : courseList) {
+//            studentRecord.addCourse(c);
+//        }
+//    }
+//
+//    public void viewAll() {
+//        List<Course> courses = studentRecord.getCourseList();
+//        for (Course c : courses) {
+//            printCourse(c);
+//
+//        }
+//        add(exit);
+//        exit.setHorizontalAlignment(JButton.CENTER);
+//    }
+//
+//    public void viewCompleted() {
+//        List<Course> courses = studentRecord.getCourseList();
+//        for (Course c : courses) {
+//            c.checkIsCompleted();
+//            if (c.getIsCompleted()) {
+//                printCourse(c);
+//            }
+//        }
+//        add(exit);
+//        exit.setHorizontalAlignment(JButton.CENTER);
+//    }
+//
+//    public void viewIncomplete() {
+//        List<Course> courses = studentRecord.getCourseList();
+//        for (Course c : courses) {
+//            c.checkIsCompleted();
+//            if (!c.getIsCompleted()) {
+//                printCourse(c);
+//            }
+//        }
+//        add(exit);
+//        exit.setHorizontalAlignment(JButton.CENTER);
+//    }
+//
+//    public void printCourse(Course c) {
+//        recordPanel = new JPanel();
+//        recordPanel.setLayout(new GridLayout(1, 2));
+//
+//
+//        JLabel curName = new JLabel();
+//        curName.setText(c.getCourseName());
+//
+//        JLabel curCredits = new JLabel();
+//        curCredits.setText(Integer.toString(c.getCredits()));
+//
+//        recordPanel.add(curName);
+//        curName.setPreferredSize(new Dimension(50, 1));
+//        curName.setHorizontalAlignment(JLabel.CENTER);
+//        recordPanel.add(curCredits);
+//        curCredits.setPreferredSize(new Dimension(50, 1));
+//        curCredits.setHorizontalAlignment(JLabel.CENTER);
+//
+//        add(recordPanel);
+//        add(new JSeparator());
+//
+//
+//    }
+//
+//
+//    // saves student record before exiting
+////    public void saveBeforeExit() {
+////        addWindowListener(new WindowAdapter() {
+////            public void windowClosing(WindowEvent e) {
+////                try {
+////                    jsonWriter.open();
+////                    jsonWriter.write(studentRecord);
+////                    jsonWriter.close();
+////                } catch (FileNotFoundException exception) {
+////                    System.out.println("Unable to write to file: " + JSON_STORE);
+////                }
+////            }
+////        });
+////
+////    }
+//
+//    public void saveBeforeExit() {
+//        try {
+//            jsonWriter.open();
+//            jsonWriter.write(studentRecord);
+//            jsonWriter.close();
+//        } catch (FileNotFoundException exception) {
+//            System.out.println("Unable to write to file: " + JSON_STORE);
+//        }
+//    }
+//
+//
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        String button = e.getActionCommand();
+//
+//        if (button.equals("Exit")) {
+//            saveBeforeExit();
+//            setVisible(false);
+//        }
+//    }
+//}
