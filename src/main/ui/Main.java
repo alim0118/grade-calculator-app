0df1fc38710413d3b0967cf594d9abe96bf9a161
alim1801 <Annabellim0118@gmail.com>
2022-11-17 19:11:17 -0800

    phase 3: mainGUI fixed checkstyle

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -6,15 +6,18 @@ import java.io.FileNotFoundException;
 // new grade calculator app
 public class Main {
     public static void main(String[] args) {
+
+        // from phase 2: run ui
+        /*
         try {
             new GradeCalculatorApp();
         } catch (FileNotFoundException e) {
             System.out.println("Error occured: file not file");
         }
+         */
 
-        //SwingUtilities.invokeLater(MainGUI::new);
-
-        //new MainGUI();
+        // phase 3: run gui
+        new MainGUI();
     }
 
 }
--- src/main/ui/MainGUI.java
+++ src/main/ui/MainGUI.java
@@ -1,33 +1,20 @@
 package ui;
 
-import model.StudentRecord;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MainGUI extends JFrame implements ActionListener {
-   private StudentRecord studentRecord;
-   //private StudentRecordPanel studentRecordPanel;
-   //private CoursePanel coursePanel;
-   //private CategoryPanel categoryPanel;
-   private JPanel panel;
-   private JLabel label;
-   private JButton buttonNew;
-   private JButton buttonOld;
-   private JButton buttonAdd;
+    private JPanel panel;
+    private JLabel label;
+    private JButton buttonNew;
+    private JButton buttonOld;
 
 
     public MainGUI() {
         super("Grade Calculator App");
         this.setSize(500, 500);
-        //this.setResizable(false);
-
-        studentRecord = new StudentRecord(1);
-        //studentRecordPanel = new StudentRecordPanel();
-        //coursePanel = new CoursePanel();
-        //categoryPanel = new CategoryPanel();
 
         homePage();
 
@@ -42,6 +29,23 @@ public class MainGUI extends JFrame implements ActionListener {
         label = new JLabel("Welcome to the Grade Calculator App. Please select: ");
         labelPanel.add(label);
 
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(200, 200));
+        panel.setBackground(Color.WHITE);
+        panel.add(labelPanel, BorderLayout.PAGE_START);
+
+        panel.add(addButton(), BorderLayout.CENTER);
+
+        addVisual();
+
+        this.add(panel, BorderLayout.CENTER);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+
+    public JPanel addButton() {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout());
         buttonPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
@@ -56,27 +60,10 @@ public class MainGUI extends JFrame implements ActionListener {
 
         buttonPanel.add(buttonNew);
         buttonPanel.add(buttonOld);
-
-        panel = new JPanel();
-        panel.setPreferredSize(new Dimension(200, 200));
-        panel.setBackground(Color.WHITE);
-        panel.add(labelPanel, BorderLayout.PAGE_START);
-
-        panel.add(buttonPanel, BorderLayout.CENTER);
-
-        addVisual();
-
-        this.add(panel, BorderLayout.CENTER);
-
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-
+        return buttonPanel;
     }
 
     public void addVisual() {
-        // already have frame
-        //JFrame f= new JFrame("Panel with image");
-
         JPanel imagePanel = new JPanel();
         imagePanel.setLayout(new FlowLayout());
         imagePanel.setBorder(BorderFactory.createEmptyBorder(5, 20, 5, 20));
@@ -89,14 +76,10 @@ public class MainGUI extends JFrame implements ActionListener {
         imageIcon = new ImageIcon(resize);
         JLabel imageLabel = new JLabel(imageIcon);
 
-        //imageLabel.setBorder(BorderFactory.createEmptyBorder(50, 20, 5, 20));
         imageLabel.setPreferredSize(new Dimension(350, 350));
-        //does nothing
-        //imageLabel.setBorder(BorderFactory.createEmptyBorder(50, 20, 5, 20));
         imagePanel.add(imageLabel);
 
         panel.add(imagePanel, BorderLayout.PAGE_END);
-
 
     }
 
@@ -105,28 +88,14 @@ public class MainGUI extends JFrame implements ActionListener {
         String button = e.getActionCommand();
 
         if (button.equals("New")) {
-            // go to "add course" panel (course panel)
-            // add somewhere -> to after new
             dispose();
-            //new MenuFrame();
             new StudentRecordFrame();
 
-        }
-        if (button.equals("Returning")) {
-            // go to student record panel
+        } else {
             dispose();
             new ReturnStudentRecordFrame();
-        } else {
-            // have popup asking number of categories
-
-
         }
-    }
 
-
-
-    public static void main(String[] args) {
-       new MainGUI();
     }
 }
 
